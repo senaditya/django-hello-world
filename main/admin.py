f517aeeebf6d5619b54c35634375b18901c8316c
aditya <aditya.sen1hl@gmail.com>
2024-05-04 00:00:29 +0430

    added home

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Tutorial
 
 # Register your models here.
+
+
+class TutorialAdmin(admin.ModelAdmin):
+    # fields = ["published", "title", "content"]
+    fieldsets = [
+        ("Title/Date", {"fields": ["title", "published"]}),
+        ("Content", {"fields": ["content"]}),
+    ]
+    
+    # formfield_overrides = {models.TextField: {"widget": QuillField()}}
+
+
+admin.site.register(Tutorial, TutorialAdmin)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django_quill.fields import QuillField
+
 
 # Create your models here.
 class Tutorial(models.Model):
-    title=models.CharField(max_length=200)
-    content=models.TextField()
-    published=models.DateTimeField("date published")
+    title = models.CharField(max_length=200)
+    # content=models.TextField()
+    content = QuillField()
+    published = models.DateTimeField("date published", default=timezone.now())
 
     def __str__(self):
         return self.title
